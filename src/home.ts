3ada9cd1ccfc2d162271a7f86c2a1fd5dd68bcd3
Kelly Harrison <kelly@sbsoftwareshop.com>
2016-07-08 19:29:21 -0400

    Updated home controller

--- src/home.ts
+++ src/home.ts
@@ -1,6 +1,7 @@
 //import {computedFrom} from 'aurelia-framework';
 
 export class Countdown {
+  title: string;
   deadline: Date;
   total: number;
   Seconds: number;
@@ -14,16 +15,7 @@ export class Countdown {
     var timeinterval: number = setInterval(this.getTimeRemaining, 1000);
   }
 
-  //Getters can't be directly observed, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below
-  //as well as the corresponding import above.
-  //@computedFrom('firstName', 'lastName')
-
-
-  getTimeRemaining() {
-    console.log(this.deadline.toString());
-
+  public getTimeRemaining = () => {
     this.total = Date.parse(this.deadline.toString()) - Date.parse(new Date().toString());
     this.Seconds = Math.floor((this.total / 1000) % 60);
     this.Minutes = Math.floor((this.total / 1000 / 60) % 60);
